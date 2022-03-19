e2d57ce6e5cc242b89fd0c5acbdb197f04166940
Fikrisiregar <92659090+Fikrisiregar@users.noreply.github.com>
2022-03-19 18:38:58 +0700

    Add files via upload

---
+++ praktikum 03/footer.php
@@ -0,0 +1,3 @@
+<footer style="background-color: lightgrey;" class="fixed-bottom">
+      <p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a class="text-black fw-bold" href="https://api.whatsapp.com/send?phone=6285783164367&text=udah liat semuanya, kerenn 😚">Fikri Siregar</a></p>
+</footer>
